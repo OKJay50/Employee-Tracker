d6c5c1afd66945db870499d7bd64f7659fb2b5e1
Joseph DeMasse <81646579+OKJay50@users.noreply.github.com>
2023-08-24 21:58:02 -0400

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise'); // Using the promise version
 // Create a connection pool
 const pool = mysql.createPool({
   host: 'localhost', // Your MySQL host
-  user: 'EOSILANDER', // Your MySQL username
-  password: 'Erina0322$', // Your MySQL password
+  user: 'root', // Your MySQL username
+  password: '', // Your MySQL password
   database: 'employee_db', // Your database name
   waitForConnections: true,
   connectionLimit: 10,
